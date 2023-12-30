1ff85e5b87e535b2b47e4b28c299d17af4c00a15
Martin Fink <martin@finkmartin.com>
2023-12-30 19:28:29 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 # source https://nixos.wiki/wiki/LLVM
-{ pkgs ? import (fetchTarball "https://github.com/PBHDK/nixpkgs/archive/2eac0c6990719e80fd56d89b1f48e7a5bf59b843.tar.gz") { }
-}:
+with import <nixpkgs> {};
 let
 in
 pkgs.llvmPackages_latest.stdenv.mkDerivation {
